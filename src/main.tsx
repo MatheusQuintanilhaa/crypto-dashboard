050145e0ae6a45a1fb5ed5abfb8626a3d27658d1
Matheus Quintanilha <matheussantos.quintanilha@gmail.com>
2025-07-08 14:34:32 -0300

    Fix: Corrigir imports de utils e configurações do TypeScript para Vite
    - Atualizou tsconfig.json para configuração padrão do Vite
    - Criou tsconfig.node.json necessário para o Vite
    - Corrigiu imports '../lib/utils' para '../../lib/utils' nos componentes UI
    - Removeu referências ao Next.js do tsconfig
    - Build local agora funciona corretamente

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "../app/globals.css";
+import "./globals.css";
 import { QueryProvider } from "./components/query-provider";
 import { ThemeProvider } from "./contexts/theme-context";
 
