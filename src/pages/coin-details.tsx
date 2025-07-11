a743b2e098fc916586f0948fe806ecc05970c4a3
Matheus Quintanilha <matheussantos.quintanilha@gmail.com>
2025-07-11 16:24:43 -0300

    fix: ensure import correction is applied - added comment to force cache refresh

--- src/pages/coin-details.tsx
+++ src/pages/coin-details.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+// Fixed import issue for Vercel deployment
 import { useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { Button } from "../components/ui/button";
